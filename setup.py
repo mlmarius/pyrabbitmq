918ade0618e55547dea53eab1b64d032dc0a774e
Liviu Marius Manea <mlmarius@yahoo.com>
2016-11-08 12:50:04 +0000

    added url to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='pyrabbitmq',
       version='1.0',
       description="My Python to RabbitMQ bridge classes",
       author="Liviu Manea",
+      url="https://github.com/mlmarius/pyrabbitmq",
       license="MIT",
       packages=['pyrabbitmq'],
       zip_safe=False
